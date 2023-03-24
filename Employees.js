d3b8f7641914b88436d1211a4bec23a56884e64c
Enriko Raymundu <116963958+Enrikokokoko@users.noreply.github.com>
2023-03-24 17:58:22 +0200

    Rewrite JavaScript to TypeScript

--- Employees.js
+++ Employees.js
@@ -1,69 +1,53 @@
-const employees = [
-    { id: 11, name: 'Name_1', managerId: 24 },
-    { id: 12, name: 'Name_2', managerId: 24 },
-    { id: 13, name: 'Name_3', managerId: 11 },
-    { id: 14, name: 'Name_4', managerId: 22 },
-    { id: 15, name: 'Name_5', managerId: 24 },
-    { id: 16, name: 'Name_6', managerId: 30 },
-    { id: 17, name: 'Name_7', managerId: 19 },
-    { id: 18, name: 'Name_8', managerId: 24 },
-    { id: 19, name: 'Name_9', managerId: 12 },
-    { id: 20, name: 'Name_10', managerId: 12 },
-    { id: 21, name: 'Name_11', managerId: 25 },
-    { id: 22, name: 'Name_12', managerId: 26 },
-    { id: 23, name: 'Name_13', managerId: 27 },
-    { id: 24, name: 'Name_14', managerId: 11 },
-    { id: 25, name: 'Name_15', managerId: 13 },
-    { id: 26, name: 'Name_16', managerId: 15 },
-    { id: 27, name: 'Name_17', managerId: 15 },
-    { id: 28, name: 'Name_18', managerId: 29 },
-    { id: 29, name: 'Name_19', managerId: 30 },
-    { id: 30, name: 'Name_20', managerId: 24 },
-  ]
+// const employees = [
+//     { id: 11, name: 'Name_1', managerId: 24 },
+//     { id: 12, name: 'Name_2', managerId: 24 },
+//     { id: 13, name: 'Name_3', managerId: 11 },
+//     { id: 14, name: 'Name_4', managerId: 22 },
+//     { id: 15, name: 'Name_5', managerId: 24 },
+//     { id: 16, name: 'Name_6', managerId: 30 },
+//     { id: 17, name: 'Name_7', managerId: 19 },
+//     { id: 18, name: 'Name_8', managerId: 24 },
+//     { id: 19, name: 'Name_9', managerId: 12 },
+//     { id: 20, name: 'Name_10', managerId: 12 },
+//     { id: 21, name: 'Name_11', managerId: 25 },
+//     { id: 22, name: 'Name_12', managerId: 26 },
+//     { id: 23, name: 'Name_13', managerId: 27 },
+//     { id: 24, name: 'Name_14', managerId: 11 },
+//     { id: 25, name: 'Name_15', managerId: 13 },
+//     { id: 26, name: 'Name_16', managerId: 15 },
+//     { id: 27, name: 'Name_17', managerId: 15 },
+//     { id: 28, name: 'Name_18', managerId: 29 },
+//     { id: 29, name: 'Name_19', managerId: 30 },
+//     { id: 30, name: 'Name_20', managerId: 24 },
+//   ]
 
-function getUserById(userId){
-  return employees.find(l =>  {
-    return l.id === userId
-   }) 
- }
-console.log(getUserById(12));
+// function getUserById(userId){
+//   return employees.find(l =>  {
+//     return l.id === userId
+//    }) 
+//  }
+// console.log(getUserById(12));
 
-function getManagerEmplyees(managerId) {
-  return employees.filter((value) => {
-      return value.managerId === managerId
-  })
-}
+// function getManagerEmplyees(managerId) {
+//   return employees.filter((value) => {
+//       return value.managerId === managerId
+//   })
+// }
 
-console.log(getManagerEmplyees(11));
-
-function getManagerForMap(managerId) {
-  return employees.filter((value) => {
-      return value.managerId === managerId
-  })
-}
-  
-function getEmplyeesMap(){
-    let result = {};
-    employees.forEach(value => {
-      result[value.id] = getManagerForMap(value.id)
-    })
-    return result
- }
-  
- const Res = getEmplyeesMap()
- console.log(Res);
+// console.log(getManagerEmplyees(11));
 
 // function getEmplyeesMap(){
-//    let result = {};
-//    employees.forEach(value => {
-
-//     let emp = result[value.id]
-//     let managers = result[value.managerId] 
-
-//     result[value.id] = []
-//    })
-//    return result
+//   const result = {}
+  
+//   employees.forEach((user) => {
+//     if (result[user.managerId] === undefined) {
+//       result[user.managerId] = [user]
+//     } else {
+//       result[user.managerId].push(user)
+//     }
+//   })
+  
+//   return result
 // }
- 
-// const tt = getEmplyeesMap()
-// console.log(tt);
+
+// console.log(getEmplyeesMap())
---
+++ Employees.ts
@@ -0,0 +1,56 @@
+type Person = { id: number, name: string, managerId: number };
+type Users = { managerId: object };
+
+const employees : Array<Person> = [
+    { id: 11, name: 'Name_1', managerId: 24 },
+    { id: 12, name: 'Name_2', managerId: 24 },
+    { id: 13, name: 'Name_3', managerId: 11 },
+    { id: 14, name: 'Name_4', managerId: 22 },
+    { id: 15, name: 'Name_5', managerId: 24 },
+    { id: 16, name: 'Name_6', managerId: 30 },
+    { id: 17, name: 'Name_7', managerId: 19 },
+    { id: 18, name: 'Name_8', managerId: 24 },
+    { id: 19, name: 'Name_9', managerId: 12 },
+    { id: 20, name: 'Name_10', managerId: 12 },
+    { id: 21, name: 'Name_11', managerId: 25 },
+    { id: 22, name: 'Name_12', managerId: 26 },
+    { id: 23, name: 'Name_13', managerId: 27 },
+    { id: 24, name: 'Name_14', managerId: 11 },
+    { id: 25, name: 'Name_15', managerId: 13 },
+    { id: 26, name: 'Name_16', managerId: 15 },
+    { id: 27, name: 'Name_17', managerId: 15 },
+    { id: 28, name: 'Name_18', managerId: 29 },
+    { id: 29, name: 'Name_19', managerId: 30 },
+    { id: 30, name: 'Name_20', managerId: 24 },
+  ]
+
+function getUserById(userId: number ): Person | undefined {
+  return employees.find((l): boolean =>  {
+    return l.id === userId
+   }) 
+ }
+console.log(getUserById(12));
+
+function getManagerEmplyees(managerId: number ): Array<Person> {
+  return employees.filter((value): boolean => {
+      return value.managerId === managerId
+  })
+}
+
+console.log(getManagerEmplyees(11));
+
+function getEmplyeesMap(): object | null {
+  const result = (o: object | null): void => {}
+  
+  employees.forEach((user) : Array<Users> | void => {
+    if (result[user.managerId] === undefined) {
+      result[user.managerId] = [user]
+    } else {
+      result[user.managerId].push(user)
+    }
+  })
+  
+  return result
+}
+
+console.log(getEmplyeesMap())
